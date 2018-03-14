6fcbb7124d416b91b626be817bc942191fcef984
Casper Lamboo <casperlamboo@gmail.com>
2018-03-14 22:52:00 +0100

    update script

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const PropTypes = require('proptypes').default;
 
-module.exports.propTypesShape = PropTypes.shape({
+const propTypesShape = module.exports.propTypesShape = PropTypes.shape({
   pending: PropTypes.bool.isRequired,
   error: PropTypes.bool.isRequired,
   ready: PropTypes.bool.isRequired,
   data: PropTypes.any
 });
 
-module.exports.initialState = { pending: false, error: false, ready: false, data: null };
+const initialState = module.exports.initialState = {
+  pending: false,
+  error: false,
+  ready: false,
+  data: null
+};
 
-module.exports.createPromiseAction = function(promise, actionType, { onSuccess, onError } = {}) {
+const createPromiseAction = module.exports.createPromiseAction = function(promise, actionType, { onSuccess, onError } = {}) {
   return (...args) => {
     return (dispatch) => {
       return dispatch({
@@ -26,7 +31,7 @@ module.exports.createPromiseAction = function(promise, actionType, { onSuccess, 
   };
 }
 
-module.exports.createPromiseReducer = function(actionType) {
+const createPromiseReducer = module.exports.createPromiseReducer = function(actionType) {
   const pendingActionType = `${actionType}_PENDING`;
   const fulfilledActionType = `${actionType}_FULFILLED`;
   const rejectedActionType = `${actionType}_REJECTED`;
